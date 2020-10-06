fe148d7d1e055a8d641d424bd118cdeb6b1f2ee4
muhadbk0 <muhadbk0@gmail.com>
2020-10-06 22:42:59 +0530

    SSL added

--- README.md
+++ README.md
@@ -2,3 +2,10 @@
 file storage application
 
 API Documentaion: https://documenter.getpostman.com/view/1550185/TVRedqGW
+
+Live Site: https://file-storage-application.vercel.app/
+
+admin User
+----------
+user name: admin@mail.com
+password: 123456
--- react-app/src/const.js
+++ react-app/src/const.js
@@ -1,4 +1,3 @@
-export const BASE_URL='http://3.6.102.94:8088'; // 'http://localhost:3001' //
+export const BASE_URL= 'https://file.technewwings.com' // 'https://localhost:8088' // 'http://3.6.102.94:8088'; //
 export const API_END_POINT='/api/v1';
 export const URL_PATH='/file-uploads/';
-
